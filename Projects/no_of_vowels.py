2aab30f59bde38f83cf688728cba191278f2e04f
Atharv Sudrik <atharvsudrik2708@gmail.com>
2026-06-12 23:24:34 +0530

    Add files via upload

---
+++ Projects/no_of_vowels.py
@@ -0,0 +1,16 @@
+str = input ('Enter String:')
+count = 0
+
+for i in str:
+    if (i == "a" ):
+        count += 1
+    elif(i == "e" ):
+        count +=1
+    elif(i == "i" ): 
+        count +=1
+    elif(i == "o" ):
+        count += 1
+    elif(i == "u"):
+        count += 1
+        
+print ("No of vowels :",count)
